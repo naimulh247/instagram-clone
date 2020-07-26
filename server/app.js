ce6b58b0fcf8c1393c174107847f7744fe9e3a26
Naimul Hasan <naimulh247@gmail.com>
2020-07-25 21:23:16 -0400

    server/app.js:5: -const customMiddleware = ()=>{ server/app.js:5: +const customMiddleware = (req, res, next)=>{

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app  = express()
 const port = 5000
 
-const customMiddleware = ()=>{
+const customMiddleware = (req, res, next)=>{
     console.log("middleware executed!")
 }
 
