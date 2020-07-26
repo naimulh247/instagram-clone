5a730832c7f1c64595b6c6696b6664398d520bd6
Naimul Hasan <naimulh247@gmail.com>
2020-07-25 22:00:42 -0400

    server/app.js:6: + server/app.js:7: + server/app.js:8: + server/app.js:9: + server/app.js:10: +

--- server/app.js
+++ server/app.js
@@ -3,6 +3,11 @@ const app  = express()
 const port = 5000
 const mongooes = require('mongoose')
 
+
+
+
+
+
 app.listen(port, ()=>{
     console.log("Server is running on, http://localhost:"+port)
 })
