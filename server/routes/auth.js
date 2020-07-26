d03aac1b487d53046fb62a1886eeb3aa87b87443
Naimul Hasan <naimulh247@gmail.com>
2020-07-26 01:48:37 -0400

    server/routes/auth.js:58: -                        const token = server/routes/auth.js:58: +                        const token = jwt.se

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -55,7 +55,7 @@ router.post('/signin', (req, res)=>{
                     if(doMatch){
                         res.json({message:"successfully signed in"})
 
-                        const token = 
+                        const token = jwt.se
                     }
                     else{
                         return res.json({message:"Incorrect email or password"})
