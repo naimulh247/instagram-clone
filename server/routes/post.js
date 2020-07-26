205ecc0d060f36bc4c1e05d926660914490fea3a
Naimul Hasan <naimulh247@gmail.com>
2020-07-26 15:01:19 -0400

    server/routes/post.js:5: - server/routes/post.js:5: +const Post = mongoose.model('Post')

--- server/routes/post.js
+++ server/routes/post.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const requireLogin = require('../middleware/requireLogin')
-
+const Post = mongoose.model('Post')
 
 
 router.post('/createpost',requireLogin, (req, res)=>{
