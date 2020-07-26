a6a8215a46d2373e45b65b57a85f7a932998dcf0
Naimul Hasan <naimulh247@gmail.com>
2020-07-25 23:36:01 -0400

    server/routes/auth.js:3: -const mongoose = require('mongoose') server/routes/auth.js:3: +const mongoose = require('mongoose') server/routes/auth.js:4: +const User =

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const mongoose = require('mongoose') 
+const mongoose = require('mongoose')
+const User =  
 
 
 router.get('/', (req, res)=>{
