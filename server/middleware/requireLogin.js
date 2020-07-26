f48f545776c48ef406bd0b18e704f7e09256e22e
Naimul Hasan <naimulh247@gmail.com>
2020-07-26 11:55:42 -0400

    server/middleware/requireLogin.js:7: -        res.status(401).json({error:"you must be logged in"}) server/middleware/requireLogin.js:7: +       return res.status(401).json({error:"you must be logged in"})

--- server/middleware/requireLogin.js
+++ server/middleware/requireLogin.js
@@ -4,7 +4,7 @@ const {JWT_SECRET} = require('../keys')
 module.exports=(req,res,next)=>{
     const {authorization} = req.headers
     if(!authorization){
-        res.status(401).json({error:"you must be logged in"})
+       return res.status(401).json({error:"you must be logged in"})
     }
     const token = authorization.replace("Bearer", "")
     jwt.verify(token,JWT_SECRET,(err, payload)=>{
