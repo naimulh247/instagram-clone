a155404cb6dfbea502ecb9679f15517952337350
Naimul Hasan <naimulh247@gmail.com>
2020-07-25 23:16:45 -0400

    server/routes/auth.js:5: +var jsonParser = bodyParser.json() server/routes/auth.js:6: +

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const bodyParser = require('body-parser')
 
+var jsonParser = bodyParser.json()
+
 
 router.get('/', (req, res)=>{
     res.send("Router works")
