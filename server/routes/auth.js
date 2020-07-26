d09d53b896273fae83582e6c4379e477e1ea7eca
Naimul Hasan <naimulh247@gmail.com>
2020-07-26 01:47:58 -0400

    server/routes/auth.js:7: +.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const User = mongoose.model("User") 
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+.
 
 
 router.post('/signup', (req, res)=>{
