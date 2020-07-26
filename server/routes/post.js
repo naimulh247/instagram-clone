7b6bdcaa4084e55c12710f11f3e62dc9c7afac44
Naimul Hasan <naimulh247@gmail.com>
2020-07-26 14:28:40 -0400

    server/routes/post.js:4: -const requireLogin = require('../middleware/requireLogin')

--- server/routes/post.js
+++ server/routes/post.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const requireLogin = require('../middleware/requireLogin')
-const requireLogin = require('../middleware/requireLogin')
 
 
 
