364adcbf79de5162d1fe4d192fefca659735c4bc
Naimul Hasan <naimulh247@gmail.com>
2020-07-26 14:25:39 -0400

    server/routes/post.js:4: +const requireLogin = require('../middleware/requireLogin') server/routes/post.js:5: +cosnt requireLogin = require('../middleware/requireLogin') server/routes/post.js:9: - server/routes/post.js:9: -router.post('/createpost', (req, res)=>{ server/routes/post.js:9: +router.post('/createpost',requireLogin, (req, res)=>{

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const requireLogin = require('../middleware/requireLogin')
+cosnt requireLogin = require('../middleware/requireLogin')
 
 
 
-
-router.post('/createpost', (req, res)=>{
+router.post('/createpost',requireLogin, (req, res)=>{
     const {title, body} = req.body
     if(!title || !body){
         return res.status(422).json({error:"please add all fields"})
