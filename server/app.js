0e5bd79eb7e4c3c6e302ff9fb63295e94697f406
Naimul Hasan <naimulh247@gmail.com>
2020-07-25 22:01:11 -0400

    server/app.js:5: -const {MONGOURL} server/app.js:5: +const {MONGOURL} = require

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app  = express()
 const port = 5000
 const mongooes = require('mongoose')
-const {MONGOURL} 
+const {MONGOURL} = require 
 
 
 mongooes.connect
