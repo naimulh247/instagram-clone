949705a5d8e99698fe3c20fb01d2ebdd23a6d7cd
Naimul Hasan <naimulh247@gmail.com>
2020-07-25 23:10:45 -0400

    server/app.js:3: -const bo server/app.js:3: +const  bodyParser = require('body-parser')

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const bo
+const  bodyParser = require('body-parser')
 const mongooes = require('mongoose')
 const {MONGOURL} = require('./keys') 
 const port = 5000
