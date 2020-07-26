b5cee8c1fc1edcd276e5a22c34251b0f53f02178
Naimul Hasan <naimulh247@gmail.com>
2020-07-25 21:17:24 -0400

    app.js:12: -    console.log("Server is running on, http://localhost:") app.js:12: +    console.log("Server is running on, http://localhost:",port)

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.get('/', (req, res)=>{
 })
 
 app.listen(port, ()=>{
-    console.log("Server is running on, http://localhost:")
+    console.log("Server is running on, http://localhost:",port)
 })
