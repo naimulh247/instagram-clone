87c16f74a0850e7054c2b6c3b96c4b4ee41c41b3
Naimul Hasan <naimulh247@gmail.com>
2020-07-25 22:00:24 -0400

    server/app.js:4: -const mongooes server/app.js:4: +const mongooes = req

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app  = express()
 const port = 5000
-const mongooes 
+const mongooes = req
 
 app.listen(port, ()=>{
     console.log("Server is running on, http://localhost:"+port)
