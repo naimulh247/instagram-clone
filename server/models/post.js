109c43af9e6768da08666c7721747b96043835a8
Naimul Hasan <naimulh247@gmail.com>
2020-07-26 13:31:35 -0400

    server/models/post.js:1: +const mongoose = require('mongoose') server/models/post.js:2: + server/models/post.js:3: + server/models/post.js:4: +const postSchema = new mongoose.Schema({ server/models/post.js:5: +    title:{ server/models/post.js:6: +        type:String, server/models/post.js:7: +        required:true server/models/post.js:8: +    }, server/models/post.js:9: +    body:{ server/models/post.js:10: +        type:String, server/models/post.js:11: +        required:true server/models/post.js:12: +    }, server/models/post.js:13: +    photo:{ server/models/post.js:14: +        type:String, server/models/post.js:15: +        default:"no photo" server/models/post.js:16: +    } server/models/post.js:17: +}) server/models/post.js:18:  No newline at end of file

---
+++ server/models/post.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+
+const postSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    body:{
+        type:String,
+        required:true
+    },
+    photo:{
+        type:String,
+        default:"no photo"
+    }
+}) 
