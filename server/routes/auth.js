400cd32472c0b033fb256e60726948cab8627573
Naimul Hasan <naimulh247@gmail.com>
2020-07-26 01:27:40 -0400

    server/routes/auth.js:5: +const bcrypt = require('bcryptjs') server/routes/auth.js:7: -router.get('/', (req, res)=>{ server/routes/auth.js:7: -    res.send("Router works") server/routes/auth.js:7: -}) server/routes/auth.js:19: -            const user = new User({ server/routes/auth.js:19: -                email, server/routes/auth.js:19: -                password, server/routes/auth.js:19: -                name server/routes/auth.js:19: -            }) server/routes/auth.js:19: -            user.save() server/routes/auth.js:19: -                .then(user=>{ server/routes/auth.js:19: -                    res.json({message:"saved successfully"}) server/routes/auth.js:19: -                }) server/routes/auth.js:19: -                .catch(err=>{ server/routes/auth.js:19: -                    console.log(err) server/routes/auth.js:19: +            bcrypt.hash(password, 12) server/routes/auth.js:20: +                .then(hashedpassword =>{ server/routes/auth.js:21: +                    const user = new User({ server/routes/auth.js:22: +                        email, server/routes/auth.js:23: +                        password:hashedpassword, server/routes/auth.js:24: +                        name server/routes/auth.js:25: +                    }) server/routes/auth.js:26: +                    user.save() server/routes/auth.js:27: +                        .then(user=>{ server/routes/auth.js:28: +                            res.json({message:"saved successfully"}) server/routes/auth.js:29: +                        }) server/routes/auth.js:30: +                        .catch(err=>{ server/routes/auth.js:31: +                            console.log(err) server/routes/auth.js:32: +                        }) server/routes/auth.js:34: -        }) server/routes/auth.js:34: + server/routes/auth.js:35: +            }) server/routes/auth.js:36: +

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,11 +2,9 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User") 
+const bcrypt = require('bcryptjs')
 
 
-router.get('/', (req, res)=>{
-    res.send("Router works")
-})
 
 router.post('/signup', (req, res)=>{
     const {name, email, password} = req.body
@@ -18,19 +16,24 @@ router.post('/signup', (req, res)=>{
             if(savedUser){
                 return res.status(422).json({error:"user already exists"})
             }
-            const user = new User({
-                email,
-                password,
-                name
+            bcrypt.hash(password, 12)
+                .then(hashedpassword =>{
+                    const user = new User({
+                        email,
+                        password:hashedpassword,
+                        name
+                    })
+                    user.save()
+                        .then(user=>{
+                            res.json({message:"saved successfully"})
+                        })
+                        .catch(err=>{
+                            console.log(err)
+                        })
+                })
+
             })
-            user.save()
-                .then(user=>{
-                    res.json({message:"saved successfully"})
-                })
-                .catch(err=>{
-                    console.log(err)
-                })
-        })
+            
         .catch(err=>{
             console.log(err)
         })
