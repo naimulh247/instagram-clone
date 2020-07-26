99759d9b5d5addc5eac8e4ad5b1c857daef808cc
Naimul Hasan <naimulh247@gmail.com>
2020-07-26 11:55:09 -0400

    server/middleware/requireLogin.js:10: -    jwt.verify(token,JWT_SECRET,(err, payload) ) server/middleware/requireLogin.js:10: +    jwt.verify(token,JWT_SECRET,(err, payload)=>{ server/middleware/requireLogin.js:11: + server/middleware/requireLogin.js:12: +    } )

--- server/middleware/requireLogin.js
+++ server/middleware/requireLogin.js
@@ -7,5 +7,7 @@ module.exports=(req,res,next)=>{
         res.status(401).json({error:"you must be logged in"})
     }
     const token = authorization.replace("Bearer", "")
-    jwt.verify(token,JWT_SECRET,(err, payload) )
+    jwt.verify(token,JWT_SECRET,(err, payload)=>{
+        
+    } )
 }
