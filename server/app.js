80894d86a4ebeb463cac45222e4d4d356822623a
Naimul Hasan <naimulh247@gmail.com>
2020-07-25 21:59:38 -0400

    server/app.js:4: -const customMiddleware = (req, res, next)=>{ server/app.js:4: -    console.log("middleware executed!") server/app.js:4: -    next() server/app.js:4: -} server/app.js:4: - server/app.js:4: - server/app.js:4: - server/app.js:4: -app.get('/', (req, res)=>{ server/app.js:4: -    res.send("hello WOrld") server/app.js:4: -}) server/app.js:4: - server/app.js:4: -app.get('/about',customMiddleware, (req, res)=>{ server/app.js:4: -    res.send("about page") server/app.js:4: -}) server/app.js:4: -

--- server/app.js
+++ server/app.js
@@ -2,21 +2,6 @@ const express = require('express')
 const app  = express()
 const port = 5000
 
-const customMiddleware = (req, res, next)=>{
-    console.log("middleware executed!")
-    next()
-}
-
-
-
-app.get('/', (req, res)=>{
-    res.send("hello WOrld")
-})
-
-app.get('/about',customMiddleware, (req, res)=>{
-    res.send("about page")
-})
-
 
 app.listen(port, ()=>{
     console.log("Server is running on, http://localhost:"+port)
