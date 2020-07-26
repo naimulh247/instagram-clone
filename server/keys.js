35b30bad12faaf069c1f7554950861056b5a840a
Naimul Hasan <naimulh247@gmail.com>
2020-07-25 22:03:32 -0400

    server/keys.js:2: -    MONGOURL:"mongodb+srv://naimul:BhhelcnCxYo7MkvQ@cluster0.r16zq.mongodb.net/test?retryWrites=true&w=majority"; server/keys.js:2: -}; server/keys.js:2:  No newline at end of file server/keys.js:3: +    MONGOURL:"mongodb+srv://naimul:BhhelcnCxYo7MkvQ@cluster0.r16zq.mongodb.net/test?retryWrites=true&w=majority" server/keys.js:4: +} server/keys.js:5:  No newline at end of file

--- server/keys.js
+++ server/keys.js
@@ -1,3 +1,3 @@
 module.exports={
-    MONGOURL:"mongodb+srv://naimul:BhhelcnCxYo7MkvQ@cluster0.r16zq.mongodb.net/test?retryWrites=true&w=majority";
-};
+    MONGOURL:"mongodb+srv://naimul:BhhelcnCxYo7MkvQ@cluster0.r16zq.mongodb.net/test?retryWrites=true&w=majority"
+}
