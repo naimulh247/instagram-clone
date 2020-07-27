fc65af573d56f7dc86e8bd6bf17a8d0df9795ea8
Naimul Hasan <naimulh247@gmail.com>
2020-07-27 01:17:14 -0400

    server/routes/post.js:11: -        .populate("postedBy", "_id", "name") server/routes/post.js:11: +        .populate("postedBy", "_id name")

--- server/routes/post.js
+++ server/routes/post.js
@@ -8,7 +8,7 @@ const Post = mongoose.model("Post")
 
 router.get('/allpost', (req, res)=>{
     Post.find()
-        .populate("postedBy", "_id", "name")
+        .populate("postedBy", "_id name")
         .then(posts=>{
             res.json({posts})
         })
