c8bb4900dc811259478682ee520a3e29683e509f
naimulh247 <naimulh247@gmail.com>
2020-07-27 18:14:25 -0400

    client/src/components/screens/Profile.js:8: -                <img style={{width:"160px", height:"160px", borderRadius:"80px"}} client/src/components/screens/Profile.js:8: -                src="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q= client/src/components/screens/Profile.js:8: -            </div> client/src/components/screens/Profile.js:8: +                <div> client/src/components/screens/Profile.js:9: +                    <img style={{width:"160px", height:"160px", borderRadius:"80px"}} client/src/components/screens/Profile.js:10: +                    src="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=40 client/src/components/screens/Profile.js:11: +                </div> client/src/components/screens/Profile.js:13: -            <div> client/src/components/screens/Profile.js:13: -                <h4>Naimul Hasan</h4> client/src/components/screens/Profile.js:13: -            </div> client/src/components/screens/Profile.js:13: +                <div> client/src/components/screens/Profile.js:14: +                    <h4>Naimul Hasan</h4> client/src/components/screens/Profile.js:15: +                </div> client/src/components/screens/Profile.js:16: +            </div> client/src/components/screens/Profile.js:18: + client/src/components/screens/Profile.js:19: +

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -5,14 +5,18 @@ const Profile = ()=>{
     return(
         <div>
             <div>
-                <img style={{width:"160px", height:"160px", borderRadius:"80px"}} 
-                src="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80"/>
-            </div>
+                <div>
+                    <img style={{width:"160px", height:"160px", borderRadius:"80px"}} 
+                    src="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80"/>
+                </div>
 
-            <div>
-                <h4>Naimul Hasan</h4>
-            </div>
+                <div>
+                    <h4>Naimul Hasan</h4>
+                </div>
+            </div>    
         </div>
+
+          
     )
 }
 
