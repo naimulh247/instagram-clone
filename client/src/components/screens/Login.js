4c15711dde29ff4a00d4823d2aa95f88e04e0b5d
naimulh247 <naimulh247@gmail.com>
2020-07-27 14:34:41 -0400

    client/src/components/screens/Login.js:7: -            <div className="card auth-card"> client/src/components/screens/Login.js:7: +            <div className="card auth-card input-field">

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Login = ()=>{
     return(
         <div className="mycard">
-            <div className="card auth-card">
+            <div className="card auth-card input-field">
                 <h2>Instagram</h2>
                 <input
                     type="text"
