e28dc29b90566ce60bb542b63bfdfec612266f89
Naimul Hasan <naimulh247@gmail.com>
2020-07-27 02:39:10 -0400

    client/src/components/Navbar.js:2: + client/src/components/Navbar.js:3: +const NavBar = ()=>{ client/src/components/Navbar.js:4: +    return( client/src/components/Navbar.js:5: +        <nav> client/src/components/Navbar.js:6: +        <div className="nav-wrapper"> client/src/components/Navbar.js:7: +          <a href="#" className="brand-logo">Logo</a> client/src/components/Navbar.js:8: +          <ul id="nav-mobile" className="right"> client/src/components/Navbar.js:9: +            <li><a href="sass.html">Sass</a></li> client/src/components/Navbar.js:10: +            <li><a href="badges.html">Components</a></li> client/src/components/Navbar.js:11: +            <li><a href="collapsible.html">JavaScript</a></li> client/src/components/Navbar.js:12: +          </ul> client/src/components/Navbar.js:13: +        </div> client/src/components/Navbar.js:14: +      </nav> client/src/components/Navbar.js:15: +    ) client/src/components/Navbar.js:16: +} client/src/components/Navbar.js:17:  No newline at end of file

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,1 +1,16 @@
 import React, { Component } from 'react'
+
+const NavBar = ()=>{
+    return(
+        <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo">Logo</a>
+          <ul id="nav-mobile" className="right">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">JavaScript</a></li>
+          </ul>
+        </div>
+      </nav>
+    )
+}
