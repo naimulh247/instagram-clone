e7c5aa2f81b5355bd219474f5e1a7cffcc962361
naimulh247 <naimulh247@gmail.com>
2020-07-27 14:39:15 -0400

    client/src/App.js:8: -import Signin from './components/screens/Login' client/src/App.js:8: +import Signin from './components/screens/SignIn'

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import NavBar from './components/Navbar'
 import {BrowserRouter, Route} from 'react-router-dom'
 import Home from './components/screens/Home'
 import Profile from './components/screens/Profile'
-import Signin from './components/screens/Login'
+import Signin from './components/screens/SignIn'
 import Signup from './components/screens/Signup'
 
 
