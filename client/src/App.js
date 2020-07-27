ba9b1c70d40af5316130529f3c9978dfcabfb51d
Naimul Hasan <naimulh247@gmail.com>
2020-07-27 03:22:03 -0400

    client/src/App.js:6: +import Home from './components/screens/Home' client/src/App.js:7: +import Profile from './components/screens/Profile' client/src/App.js:8: +import Login from './components/screens/Login' client/src/App.js:9: +import Signup from './components/screens/Signup'

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/Navbar'
 import {BrowserRouter, Route} from 'react-router-dom'
+import Home from './components/screens/Home'
+import Profile from './components/screens/Profile'
+import Login from './components/screens/Login'
+import Signup from './components/screens/Signup'
 
 function App() {
   return (
