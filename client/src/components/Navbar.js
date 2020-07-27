325f401fd9f9e678e20d536de7e6fc2528d65737
Naimul Hasan <naimulh247@gmail.com>
2020-07-27 02:48:13 -0400

    client/src/components/Navbar.js:7: -          <a href="#" className="brand-logo">Logo</a> client/src/components/Navbar.js:7: +          <a href="#" className="brand-logo left">Logo</a>

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ const NavBar = ()=>{
     return(
         <nav>
         <div className="nav-wrapper">
-          <a href="#" className="brand-logo">Logo</a>
+          <a href="#" className="brand-logo left">Logo</a>
           <ul id="nav-mobile" className="right">
             <li><a href="sass.html">Sass</a></li>
             <li><a href="badges.html">Components</a></li>
