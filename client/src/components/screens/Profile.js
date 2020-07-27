e7214ca02fcdd8a89d3dfcc54291db0b321236da
naimulh247 <naimulh247@gmail.com>
2020-07-27 18:15:21 -0400

    client/src/components/screens/Profile.js:7: -            <div> client/src/components/screens/Profile.js:7: +            <div style={{ client/src/components/screens/Profile.js:8: +                display:"flex", client/src/components/screens/Profile.js:9: +                justifyContent:"space-around", client/src/components/screens/Profile.js:10: +                margin:"18px 0px" client/src/components/screens/Profile.js:11: +            }}>

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -4,7 +4,11 @@ import React from 'react'
 const Profile = ()=>{
     return(
         <div>
-            <div>
+            <div style={{
+                display:"flex",
+                justifyContent:"space-around",
+                margin:"18px 0px"
+            }}>
                 <div>
                     <img style={{width:"160px", height:"160px", borderRadius:"80px"}} 
                     src="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80"/>
