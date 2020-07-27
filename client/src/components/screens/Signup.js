f33bfdde3676a5fa72c1b2a444aafc5d5c28ec9b
naimulh247 <naimulh247@gmail.com>
2020-07-27 14:40:26 -0400

    client/src/components/screens/Signup.js:6: -        <h1>Signup</h1> client/src/components/screens/Signup.js:6: +        <div className="mycard"> client/src/components/screens/Signup.js:7: +            <div className="card auth-card input-field"> client/src/components/screens/Signup.js:8: +                <h2>Instagram</h2> client/src/components/screens/Signup.js:9: +                <input client/src/components/screens/Signup.js:10: +                    type="text" client/src/components/screens/Signup.js:11: +                    placeholder="name" client/src/components/screens/Signup.js:12: +                /> client/src/components/screens/Signup.js:13: +                <input client/src/components/screens/Signup.js:14: +                    type="text" client/src/components/screens/Signup.js:15: +                    placeholder="email" client/src/components/screens/Signup.js:16: +                /> client/src/components/screens/Signup.js:17: +                <input client/src/components/screens/Signup.js:18: +                    type="text" client/src/components/screens/Signup.js:19: +                    placeholder="password" client/src/components/screens/Signup.js:20: +                /> client/src/components/screens/Signup.js:21: +                 <button className="btn waves-effect waves-light #64b5f6 blue lighten-2"> client/src/components/screens/Signup.js:22: +                    Sign Up client/src/components/screens/Signup.js:23: +                </button> client/src/components/screens/Signup.js:24: +            </div> client/src/components/screens/Signup.js:25: + client/src/components/screens/Signup.js:26: + client/src/components/screens/Signup.js:27: +        </div>

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -3,7 +3,28 @@ import React from 'react'
 
 const Signup = ()=>{
     return(
-        <h1>Signup</h1>
+        <div className="mycard">
+            <div className="card auth-card input-field">
+                <h2>Instagram</h2>
+                <input
+                    type="text"
+                    placeholder="name"
+                />
+                <input
+                    type="text"
+                    placeholder="email"
+                />
+                <input 
+                    type="text"
+                    placeholder="password"
+                />
+                 <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">
+                    Sign Up
+                </button>
+            </div>
+
+
+        </div>
     )
 }
 
