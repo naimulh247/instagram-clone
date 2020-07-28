25dd52118add714fb4c9fce3623e7bd6a23aa2ac
naimulh247 <naimulh247@gmail.com>
2020-07-27 21:48:21 -0400

    client/src/components/screens/Profile.js:14: -                    src="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=40 client/src/components/screens/Profile.js:14: +                    src="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=40

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -11,7 +11,7 @@ const Profile = ()=>{
             }}>
                 <div>
                     <img style={{width:"160px", height:"160px", borderRadius:"80px"}} 
-                    src="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80"/>
+                    src="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80" alt="profiles Pic"/>
                 </div>
 
                 <div>
