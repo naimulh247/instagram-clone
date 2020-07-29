beebc4d65a1f1b7794ca21480c79aa804a3edc71
naimulh247 <naimulh247@gmail.com>
2020-07-29 16:34:26 -0400

    client/src/components/screens/Home.js:6: -        <h1>Home</h1> client/src/components/screens/Home.js:6: +        <div className="home"> client/src/components/screens/Home.js:7: + client/src/components/screens/Home.js:8: +            <div className="card home-card"> client/src/components/screens/Home.js:9: +                <h5>Naimul</h5> client/src/components/screens/Home.js:10: +                <div className="card-image"> client/src/components/screens/Home.js:11: +                    <img src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=c client/src/components/screens/Home.js:12: +                </div> client/src/components/screens/Home.js:13: +            </div> client/src/components/screens/Home.js:14: + client/src/components/screens/Home.js:15: + client/src/components/screens/Home.js:16: + client/src/components/screens/Home.js:17: +        </div>

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -3,7 +3,18 @@ import React from 'react'
 
 const Home = ()=>{
     return(
-        <h1>Home</h1>
+        <div className="home">
+
+            <div className="card home-card">
+                <h5>Naimul</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+                </div>
+            </div>
+
+
+
+        </div>
     )
 }
 
