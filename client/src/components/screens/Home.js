ba753ec69c2676ecdc5343e486790e6178a6f587
naimulh247 <naimulh247@gmail.com>
2020-07-29 16:44:12 -0400

    client/src/components/screens/Home.js:14: +                    <i className="material-icons">favorite</i> client/src/components/screens/Home.js:27: +                    <i className="material-icons">favorite</i> client/src/components/screens/Home.js:40: +                    <i className="material-icons">favorite</i> client/src/components/screens/Home.js:53: +                    <i className="material-icons">favorite</i>

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -11,6 +11,7 @@ const Home = ()=>{
                     <img src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
                 </div>
                 <div className="card-content">
+                    <i className="material-icons">favorite</i>
                     <h6>Title</h6>
                     <p>This is an amazing post</p>
                     <input type="text" placeholder="add a comment"/>
@@ -23,6 +24,7 @@ const Home = ()=>{
                     <img src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
                 </div>
                 <div className="card-content">
+                    <i className="material-icons">favorite</i>
                     <h6>Title</h6>
                     <p>This is an amazing post</p>
                     <input type="text" placeholder="add a comment"/>
@@ -35,6 +37,7 @@ const Home = ()=>{
                     <img src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
                 </div>
                 <div className="card-content">
+                    <i className="material-icons">favorite</i>
                     <h6>Title</h6>
                     <p>This is an amazing post</p>
                     <input type="text" placeholder="add a comment"/>
@@ -47,6 +50,7 @@ const Home = ()=>{
                     <img src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
                 </div>
                 <div className="card-content">
+                    <i className="material-icons">favorite</i>
                     <h6>Title</h6>
                     <p>This is an amazing post</p>
                     <input type="text" placeholder="add a comment"/>
