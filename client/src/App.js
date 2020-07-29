b0cba362fde5323c3e5b63b6c51e9cec76483800
naimulh247 <naimulh247@gmail.com>
2020-07-29 16:52:47 -0400

    client/src/App.js:10: +import CreatePost from './components/screens/CreatPost'

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Home from './components/screens/Home'
 import Profile from './components/screens/Profile'
 import Signin from './components/screens/SignIn'
 import Signup from './components/screens/Signup'
+import CreatePost from './components/screens/CreatPost'
 
 
 
