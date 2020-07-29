23c4beb4614161df801cbbf24dba7f35f9fc8373
naimulh247 <naimulh247@gmail.com>
2020-07-29 17:15:19 -0400

    client/src/components/screens/Signup.js:5: +    const [name, setName] = useState("") client/src/components/screens/Signup.js:6: +    const [password, setPassword] = useState("") client/src/components/screens/Signup.js:7: +    const [email, setEmail] = useState("") client/src/components/screens/Signup.js:8: + client/src/components/screens/Signup.js:16: +                    value={name} client/src/components/screens/Signup.js:17: +                    onChange={(e)=>setName(e.target.value)} client/src/components/screens/Signup.js:22: +                    value={email} client/src/components/screens/Signup.js:23: +                    onChange={(e)=>setEmail(e.target.value)} client/src/components/screens/Signup.js:28: +                    value={password} client/src/components/screens/Signup.js:29: +                    onChange={(e)=>setPassword(e.target.value)}

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -2,6 +2,10 @@ import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 
 const Signup = ()=>{
+    const [name, setName] = useState("")
+    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("")
+
     return(
         <div className="mycard">
             <div className="card auth-card input-field">
@@ -9,14 +13,20 @@ const Signup = ()=>{
                 <input
                     type="text"
                     placeholder="name"
+                    value={name}
+                    onChange={(e)=>setName(e.target.value)}
                 />
                 <input
                     type="text"
                     placeholder="email"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
                 />
                 <input 
                     type="text"
                     placeholder="password"
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
                 />
                  <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">
                     Sign Up
