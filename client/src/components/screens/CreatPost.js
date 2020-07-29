27b52c62ebf485cf157345e7c24bf4ca929bb655
naimulh247 <naimulh247@gmail.com>
2020-07-29 17:02:36 -0400

    client/src/components/screens/CreatPost.js:5: -        <div className="card input-field"> client/src/components/screens/CreatPost.js:5: +        <div className="card input-field" client/src/components/screens/CreatPost.js:6: +            style={{ client/src/components/screens/CreatPost.js:7: +                margin:"10px auto", client/src/components/screens/CreatPost.js:8: +                maxWidth:"500px", client/src/components/screens/CreatPost.js:9: +                padding:"20px", client/src/components/screens/CreatPost.js:10: +                textAlign:"center" client/src/components/screens/CreatPost.js:11: +            }} client/src/components/screens/CreatPost.js:12: +        >

--- client/src/components/screens/CreatPost.js
+++ client/src/components/screens/CreatPost.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const CreatePost = () =>{
     return(
-        <div className="card input-field">
+        <div className="card input-field"
+            style={{
+                margin:"10px auto",
+                maxWidth:"500px",
+                padding:"20px",
+                textAlign:"center"
+            }}
+        >
             <input type="text" placeholder="Title"/>
             <input type="text" placeholder="Body"/>
             <div className="file-field  input-field">
