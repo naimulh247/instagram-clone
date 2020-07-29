9cff9dadca13bc5b354bd26cd9caa7e7df18c56c
naimulh247 <naimulh247@gmail.com>
2020-07-29 16:42:56 -0400

    client/src/components/screens/Home.js:62: +                    <i className="material-icons">favorite</i>

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -59,6 +59,7 @@ const Home = ()=>{
                     <img src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
                 </div>
                 <div className="card-content">
+                    <i className="material-icons">favorite</i>
                     <h6>Title</h6>
                     <p>This is an amazing post</p>
                     <input type="text" placeholder="add a comment"/>
