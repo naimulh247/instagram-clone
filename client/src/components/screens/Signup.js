d02c48c19538363254a340db78d16a57fab7a5b7
naimulh247 <naimulh247@gmail.com>
2020-07-29 17:56:36 -0400

    client/src/components/screens/Signup.js:2: -import {Link} from 'react-router-dom' client/src/components/screens/Signup.js:2: +import {Link, useHistory} from 'react-router-dom'

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
 import M from 'materialize-css'
 
 const Signup = ()=>{
