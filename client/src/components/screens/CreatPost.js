dc02d48e81c92de454e72378adffa23b2ec8920c
naimulh247 <naimulh247@gmail.com>
2020-07-29 16:48:14 -0400

    client/src/components/screens/CreatPost.js:1: +import React from 'react' client/src/components/screens/CreatPost.js:2: + client/src/components/screens/CreatPost.js:3: +const CreatePost = () =>{ client/src/components/screens/CreatPost.js:4: +    return( client/src/components/screens/CreatPost.js:5: +        <div className="card input-field"> client/src/components/screens/CreatPost.js:6: +            <input type="text" placeholder="Title"/> client/src/components/screens/CreatPost.js:7: +            <input type="text" placeholder="Body"/> client/src/components/screens/CreatPost.js:8: +            <input type="text" placeholder="Title"/> client/src/components/screens/CreatPost.js:9: + client/src/components/screens/CreatPost.js:10: +        </div> client/src/components/screens/CreatPost.js:11: +    ) client/src/components/screens/CreatPost.js:12: +} client/src/components/screens/CreatPost.js:13: + client/src/components/screens/CreatPost.js:14: +export default CreatePost client/src/components/screens/CreatPost.js:15:  No newline at end of file

---
+++ client/src/components/screens/CreatPost.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CreatePost = () =>{
+    return(
+        <div className="card input-field">
+            <input type="text" placeholder="Title"/>
+            <input type="text" placeholder="Body"/>
+            <input type="text" placeholder="Title"/>
+
+        </div>
+    )
+} 
+
+export default CreatePost
