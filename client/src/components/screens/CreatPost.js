aaf36f030b83dd938dd01d9c04f21b85b1c2dd9a
naimulh247 <naimulh247@gmail.com>
2020-07-29 21:22:39 -0400

    client/src/components/screens/CreatPost.js:31: -                    <input type="file" onChange={(e)=> console.log(e.target.files)} /> client/src/components/screens/CreatPost.js:31: +                    <input type="file" onChange={(e)=> setImage(e.target.files[0])} />

--- client/src/components/screens/CreatPost.js
+++ client/src/components/screens/CreatPost.js
@@ -28,7 +28,7 @@ const CreatePost = () =>{
             <div className="file-field  input-field">
                 <div className="btn #64b5f6 blue darken-1">
                     <span>Upload Image</span>
-                    <input type="file" onChange={(e)=> console.log(e.target.files)} />
+                    <input type="file" onChange={(e)=> setImage(e.target.files[0])} />
                 </div>
                 <div className="file-path-wrapper">
                     <input className="file-path validation" type="text" />
