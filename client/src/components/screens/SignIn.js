b3371544a941ad7170f790cd1fea5e894e9e26b4
naimulh247 <naimulh247@gmail.com>
2020-07-29 21:01:03 -0400

    client/src/components/screens/SignIn.js:2: -import {Link} from 'react-router-dom' client/src/components/screens/SignIn.js:2: +import {Link, useHistory} from 'react-router-dom' client/src/components/screens/SignIn.js:5: +    const history = useHistory() client/src/components/screens/SignIn.js:6: +    const [password, setPassword] = useState("") client/src/components/screens/SignIn.js:7: +    const [email, setEmail] = useState("") client/src/components/screens/SignIn.js:8: + client/src/components/screens/SignIn.js:9: +    const PostData = () =>{ client/src/components/screens/SignIn.js:10: +        if(!/[A-Za-z0-9._%+-]+@(gmail.com)/.test(email)){ client/src/components/screens/SignIn.js:11: +            M.toast({html:"fail"}) client/src/components/screens/SignIn.js:12: +            return client/src/components/screens/SignIn.js:13: +        } client/src/components/screens/SignIn.js:14: +        fetch("/signup",{ client/src/components/screens/SignIn.js:15: +        method:"post", client/src/components/screens/SignIn.js:16: +        headers:{ client/src/components/screens/SignIn.js:17: +            "Content-Type":"application/json" client/src/components/screens/SignIn.js:18: +        }, client/src/components/screens/SignIn.js:19: +        body:JSON.stringify({ client/src/components/screens/SignIn.js:20: +            name:name, client/src/components/screens/SignIn.js:21: +            password:password, client/src/components/screens/SignIn.js:22: +            email:email client/src/components/screens/SignIn.js:23: +            }) client/src/components/screens/SignIn.js:24: +        }).then(res=>res.json()) client/src/components/screens/SignIn.js:25: +            .then(data=>{ client/src/components/screens/SignIn.js:26: +                if(data.error){ client/src/components/screens/SignIn.js:27: +                    M.toast({html:data.error, classes:"#c62828 red darken-3"}) client/src/components/screens/SignIn.js:28: +                } client/src/components/screens/SignIn.js:29: +                else{ client/src/components/screens/SignIn.js:30: +                    M.toast({html:data.message, classes:"#43a07 green darken-1"}) client/src/components/screens/SignIn.js:31: +                    history.push('/signin') client/src/components/screens/SignIn.js:32: +                } client/src/components/screens/SignIn.js:33: +            }).catch(err=>{ client/src/components/screens/SignIn.js:34: +                console.log(err) client/src/components/screens/SignIn.js:35: +            }) client/src/components/screens/SignIn.js:36: +    } client/src/components/screens/SignIn.js:44: +                    value={email} client/src/components/screens/SignIn.js:45: +                    onChange={(e)=>setEmail(e.target.value)} client/src/components/screens/SignIn.js:50: +                    value={password} client/src/components/screens/SignIn.js:51: +                    onChange={(e)=>setPassword(e.target.value)}

--- client/src/components/screens/SignIn.js
+++ client/src/components/screens/SignIn.js
@@ -1,7 +1,39 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
 
 const SignIn = ()=>{
+    const history = useHistory()
+    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("")
+    
+    const PostData = () =>{
+        if(!/[A-Za-z0-9._%+-]+@(gmail.com)/.test(email)){
+            M.toast({html:"fail"})
+            return
+        }
+        fetch("/signup",{ 
+        method:"post",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            name:name,
+            password:password,
+            email:email
+            })
+        }).then(res=>res.json())
+            .then(data=>{
+                if(data.error){
+                    M.toast({html:data.error, classes:"#c62828 red darken-3"})
+                }
+                else{
+                    M.toast({html:data.message, classes:"#43a07 green darken-1"})
+                    history.push('/signin')
+                }
+            }).catch(err=>{
+                console.log(err)
+            })
+    }
     return(
         <div className="mycard">
             <div className="card auth-card input-field">
@@ -9,10 +41,14 @@ const SignIn = ()=>{
                 <input
                     type="text"
                     placeholder="email"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
                 />
                 <input 
                     type="text"
                     placeholder="password"
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
                 />
                 <button className="btn waves-effect waves-light #64b5f6 blue darken-2">
                     Login
