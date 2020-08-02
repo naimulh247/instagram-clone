116365849efb92b267dec1eb1d07eae677fff456
naimulh247 <naimulh247@gmail.com>
2020-08-02 18:30:16 -0400

    server/routes/post.js:9: -    const {title, body} = req.body server/routes/post.js:9: -    if(!title || !body){ server/routes/post.js:9: +    const {title, body, pic} = req.body server/routes/post.js:10: +    if(!title || !body || !pic){

--- server/routes/post.js
+++ server/routes/post.js
@@ -6,8 +6,8 @@ const Post = mongoose.model("Post")
 
 
 router.post('/createpost',requireLogin, (req, res)=>{
-    const {title, body} = req.body
-    if(!title || !body){
+    const {title, body, pic} = req.body
+    if(!title || !body || !pic){
         return res.status(422).json({error:"please add all fields"})
     }
     
