1d0fea2e6413c90316530b9f363b7fdc5c1a96e5
naimulh247 <naimulh247@gmail.com>
2020-08-02 21:10:42 -0400

    client/src/App.js:18: +  const {state,dispatch} = useContext(UserContext) client/src/App.js:19: + client/src/App.js:21: -    const {state,dispatch} = useContext(UserContext)

--- client/src/App.js
+++ client/src/App.js
@@ -15,9 +15,10 @@ export const UserContext = createContext()
 
 const Routing = () =>{
   const history = useHistory()
+  const {state,dispatch} = useContext(UserContext)
+  
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("user"))
-    const {state,dispatch} = useContext(UserContext)
     
     if(user){
       dispatch({type:"USER", payload:user})
