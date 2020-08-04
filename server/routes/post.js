518c5516c414f19e4fe9a892ccc883fb6fb8f9af
naimulh247 <naimulh247@gmail.com>
2020-08-04 09:42:51 -0400

    server/routes/post.js:55: - router.put('/unlike', requireLogin,(req, res)=>{ server/routes/post.js:55: +router.put('/unlike', requireLogin,(req, res)=>{ server/routes/post.js:56: +    Post.findByIdAndUpdate(req.body.postId,{ server/routes/post.js:57: +        $pull:{likes:req.user._id} server/routes/post.js:58: +    },{ server/routes/post.js:59: +        new:true server/routes/post.js:60: +    }).exec((err, result)=>{ server/routes/post.js:61: +        if(err){ server/routes/post.js:62: +            return res.status(422).json({err}) server/routes/post.js:63: +        } server/routes/post.js:64: +        else{ server/routes/post.js:65: +            res.json(result) server/routes/post.js:66: +        } server/routes/post.js:67: +    }) server/routes/post.js:68: +}) server/routes/post.js:69: + server/routes/post.js:70: +router.put('/comment', requireLogin,(req, res)=>{ server/routes/post.js:71: +    const comment = { server/routes/post.js:72: +        text:req.body.text, server/routes/post.js:73: +        postedBy:req.user._id server/routes/post.js:74: +    }

--- server/routes/post.js
+++ server/routes/post.js
@@ -52,7 +52,26 @@ router.put('/like', requireLogin,(req, res)=>{
      })
  })
 
- router.put('/unlike', requireLogin,(req, res)=>{
+router.put('/unlike', requireLogin,(req, res)=>{
+    Post.findByIdAndUpdate(req.body.postId,{
+        $pull:{likes:req.user._id}
+    },{
+        new:true
+    }).exec((err, result)=>{
+        if(err){
+            return res.status(422).json({err})
+        }
+        else{
+            res.json(result)
+        }
+    })
+})
+
+router.put('/comment', requireLogin,(req, res)=>{
+    const comment = {
+        text:req.body.text,
+        postedBy:req.user._id
+    }
     Post.findByIdAndUpdate(req.body.postId,{
         $pull:{likes:req.user._id}
     },{
