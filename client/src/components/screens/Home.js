5c01dd2dca72dc7432be3bb61f08e793f8f80ab8
naimulh247 <naimulh247@gmail.com>
2020-08-03 22:41:43 -0400

    client/src/components/screens/Home.js:6: -    const [data, setData] = useState("") client/src/components/screens/Home.js:6: +    const [data, setData] = useState('')

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react'
 
 const Home = ()=>{
 
-    const [data, setData] = useState("")
+    const [data, setData] = useState('')
 
     useEffect(()=>{
         fetch('/allpost',{
