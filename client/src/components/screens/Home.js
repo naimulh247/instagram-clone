ca859f9d798bed657038fd58f2ab51a79aedf333
naimulh247 <naimulh247@gmail.com>
2020-08-03 22:02:43 -0400

    client/src/components/screens/Home.js:16: -                // setData(result) client/src/components/screens/Home.js:16: +                setData(result.posts)

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -13,7 +13,7 @@ const Home = ()=>{
         }).then(res =>res.json())
         .then(result=>{
                 console.log(result)
-                // setData(result)
+                setData(result.posts)
             })
     }, [])
 
