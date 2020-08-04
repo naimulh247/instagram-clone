443603a90b90ff20759b405b9e41c304bd2278f2
naimulh247 <naimulh247@gmail.com>
2020-08-04 19:16:47 -0400

    server/routes/post.js:10: +    .populate("comments.postedBy", "name")

--- server/routes/post.js
+++ server/routes/post.js
@@ -7,6 +7,7 @@ const Post = mongoose.model("Post")
 router.get('/allpost', requireLogin, (req,res)=>{
     Post.find()
     .populate("postedBy", "_id name")
+    .populate("comments.postedBy", "name")
         .then(post=>{
             res.json({post})
         })
