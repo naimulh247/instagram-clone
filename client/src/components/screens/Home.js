6ecb4f0ccc92550ea2ecba5d2ca02c8094c99558
naimulh247 <naimulh247@gmail.com>
2020-08-03 22:49:39 -0400

    client/src/components/screens/Home.js:15: -            console.log(result.post) client/src/components/screens/Home.js:15: +            // console.log(result.post) client/src/components/screens/Home.js:17: -            console.log(data) client/src/components/screens/Home.js:17: +            // console.log(data)

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -12,9 +12,9 @@ const Home = ()=>{
             }
         }).then(res=>res.json())
         .then(result=>{
-            console.log(result.post)
+            // console.log(result.post)
             setData(result.post)
-            console.log(data)
+            // console.log(data)
         })
      },[])
 
