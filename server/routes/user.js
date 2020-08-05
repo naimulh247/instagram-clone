8d877a5f4fbe9fd84729fbf52aeb923ff9ac02ba
naimulh247 <naimulh247@gmail.com>
2020-08-05 10:20:17 -0400

    server/routes/user.js:41: -      },{new:true}).select("-password").then(result=>{ server/routes/user.js:41: +      },{new:true}).then(result=>{

--- server/routes/user.js
+++ server/routes/user.js
@@ -38,7 +38,7 @@ router.put('/follow',requireLogin,(req,res)=>{
       User.findByIdAndUpdate(req.user._id,{
           $push:{following:req.body.followId}
           
-      },{new:true}).select("-password").then(result=>{
+      },{new:true}).then(result=>{
           res.json(result)
       }).catch(err=>{
           return res.status(422).json({error:err})
