8fd457d8f4fb8a23d4123b460b54d8cbfb0a900e
naimulh247 <naimulh247@gmail.com>
2020-08-04 23:06:49 -0400

    client/src/components/screens/Home.js:135: -                            <h5><Link to={"/profile/"+item.postedBy._id}>{item.postedBy.name}</Link> client/src/components/screens/Home.js:135: +                            <h5><Link to={item.postedBy._id !== state._id? "/profile/"+item.postedBy._id : "/profile" }>{item.postedBy.name}</Link>

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -132,7 +132,7 @@ const Home = ()=>{
                     // console.log(data)
                     return(
                         <div className="card home-card" key={item._id}>
-                            <h5><Link to={"/profile/"+item.postedBy._id}>{item.postedBy.name}</Link> 
+                            <h5><Link to={item.postedBy._id !== state._id? "/profile/"+item.postedBy._id : "/profile" }>{item.postedBy.name}</Link> 
                             {item.postedBy._id == state._id && <i className="material-icons" style={{float:"right"}}
                                 onClick={()=>{
                                     deletePost(item._id)
