078acd10c79a9d3ff6c8f68f418714ba3132eae3
naimulh247 <naimulh247@gmail.com>
2020-08-05 08:22:26 -0400

    server/routes/user.js:29: - server/routes/user.js:29: +router.put('follow',requireLogin, (req, res)=>{ server/routes/user.js:30: +    User.findByIdAndUpdate(req.body.followId, { server/routes/user.js:31: +        $push:{followers:req.user._id} server/routes/user.js:32: +    }, { server/routes/user.js:33: +        new:true server/routes/user.js:34: +    }, (err, result=>{ server/routes/user.js:35: +        if(err){ server/routes/user.js:36: +            return res.status(422).json({error:err}) server/routes/user.js:37: +        } server/routes/user.js:38: +        User.findById(req.user._id,{ server/routes/user.js:39: +            $push:{following:req.body.followId} server/routes/user.js:40: +        }, {new:true}).then(result=>{ server/routes/user.js:41: +            res.json(result) server/routes/user.js:42: +        }).catch(err=>{ server/routes/user.js:43: +            res.status(422).json({error:err}) server/routes/user.js:44: +        }) server/routes/user.js:45: +    }) server/routes/user.js:46: +    ) server/routes/user.js:47: +})

--- server/routes/user.js
+++ server/routes/user.js
@@ -26,7 +26,25 @@ router.get('/user/:id',requireLogin, (req, res)=>{
     })
 })
 
-
+router.put('follow',requireLogin, (req, res)=>{
+    User.findByIdAndUpdate(req.body.followId, {
+        $push:{followers:req.user._id}
+    }, {
+        new:true
+    }, (err, result=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }
+        User.findById(req.user._id,{
+            $push:{following:req.body.followId}
+        }, {new:true}).then(result=>{
+            res.json(result)
+        }).catch(err=>{
+            res.status(422).json({error:err})
+        })
+    })
+    )
+})
 
 
 module.exports = router
