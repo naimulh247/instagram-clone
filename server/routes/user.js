c940a4a96ad6f3da920f5ebb072384a24bd6b849
naimulh247 <naimulh247@gmail.com>
2020-08-04 21:38:37 -0400

    server/routes/user.js:1: +const express = require('express') server/routes/user.js:2: +const router = express.Router() server/routes/user.js:3: +const mongoose = require('mongoose') server/routes/user.js:4: +const User = mongoose.model("User") server/routes/user.js:5: +const bcrypt = require('bcryptjs') server/routes/user.js:6: +const jwt = require('jsonwebtoken') server/routes/user.js:7: +const { JWT_SECRET } = require('../keys') server/routes/user.js:8: +const requireLogin = require('../middleware/requireLogin') server/routes/user.js:9: +const Post = mongoose.model("Post") server/routes/user.js:10: + server/routes/user.js:11: + server/routes/user.js:12: + server/routes/user.js:13: + server/routes/user.js:14: + server/routes/user.js:15: + server/routes/user.js:16: + server/routes/user.js:17: +module.exports router server/routes/user.js:18:  No newline at end of file

---
+++ server/routes/user.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const User = mongoose.model("User") 
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../keys')
+const requireLogin = require('../middleware/requireLogin')
+const Post = mongoose.model("Post")
+
+
+
+
+
+
+
+module.exports router
